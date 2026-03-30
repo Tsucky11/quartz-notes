4faae6ba3d9a8e24f7da2e76faa76055ba292bc6
Satsuki <Tsucky11@users.noreply.github.com>
2026-03-30 20:49:40 +0900

    edit quartz.layout.ts
    グラフビューを削除

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -62,7 +62,6 @@ export const defaultContentPageLayout: PageLayout = {
     })
   ],
   right: [
-    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
